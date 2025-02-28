2da45147f30ce9fe98bda05271cd211a724c1d51
김선홍 <bebo007@naver.com>
2025-02-28 23:43:19 +0900

    [level 2] Title: 의상, Time: 0.11 ms, Memory: 76.8 MB -BaekjoonHub

---
+++ Java/프로그래머스/2/42578. 의상/의상.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[][] clothes) {
+        Map<String, Integer> map = new HashMap<>();
+
+        for (String[] cloth : clothes) {
+            String type = cloth[1];
+            map.put(type, map.getOrDefault(type, 0) + 1);
+        }
+
+        int answer = 1;
+
+        for (int count : map.values()) {
+            answer *= (count + 1);
+        }
+
+        return answer - 1;
+    }
+}
